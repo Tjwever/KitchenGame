a106423fb3ecb53f285e09f5296c68b19c77c2cb
Timothy Wever <timjwever@gmail.com>
2023-08-26 10:07:41 -0400

    game commit init

---
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateKitchenObject : KitchenObject
+{
+
+    public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+    public class OnIngredientAddedEventArgs : EventArgs
+    {
+        public KitchenObjectSO kitchenObjectSO;
+    }
+
+    [SerializeField] private List<KitchenObjectSO> validKicthenObjectSOList;
+
+    private List<KitchenObjectSO> kitchenObjectSOList;
+
+    private void Awake()
+    {
+        kitchenObjectSOList = new List<KitchenObjectSO>();
+    }
+
+    public bool TryAddIngredient(KitchenObjectSO kitchenObjectSO)
+    {
+        if (!validKicthenObjectSOList.Contains(kitchenObjectSO))
+        {
+            Debug.Log("Not a valid ingredient...");
+            return false;
+        }
+        if (kitchenObjectSOList.Contains(kitchenObjectSO))
+        {
+            Debug.Log("Already has this type");
+            return false;
+        } else
+        {
+            Debug.Log("Success!");
+            kitchenObjectSOList.Add(kitchenObjectSO);
+
+            OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs
+            {
+                kitchenObjectSO = kitchenObjectSO
+            });
+            return true;
+        }
+    }
+
+    public List<KitchenObjectSO> GetKitchenObjectSOList()
+    {
+        return kitchenObjectSOList;
+    }
+
+}
---
+++ Assets/Scripts/UI/TutorialUI.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Search;
+using UnityEngine;
+
+public class TutorialUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI keyMoveUpText;
+    [SerializeField] private TextMeshProUGUI keyMoveDownText;
+    [SerializeField] private TextMeshProUGUI keyMoveLeftText;
+    [SerializeField] private TextMeshProUGUI keyMoveRightText;
+    [SerializeField] private TextMeshProUGUI keyInteractText;
+    [SerializeField] private TextMeshProUGUI keyInteractAlternate;
+    [SerializeField] private TextMeshProUGUI keyPause;
+    [SerializeField] private TextMeshProUGUI keyGamepadInteractText;
+    [SerializeField] private TextMeshProUGUI keyGamepadInteractAlternateText;
+    [SerializeField] private TextMeshProUGUI keyGamepadPause;
+
+    private void Start()
+    {
+        GameInput.Instance.OnBindingRebind += GameInput_OnBindingRebind;
+        KitchenGameManager.Instance.OnStateChanged += KitchenGameManager_OnStateChanged;
+
+        UpdateVisual();
+
+        Show();
+    }
+
+    private void KitchenGameManager_OnStateChanged(object sender, System.EventArgs e)
+    {
+        if(KitchenGameManager.Instance.IsCountdownToStartActive())
+        {
+            Hide();
+        }
+    }
+
+    private void GameInput_OnBindingRebind(object sender, System.EventArgs e)
+    {
+        UpdateVisual();
+    }
+
+    private void UpdateVisual()
+    {
+        keyMoveUpText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Move_Up);
+        keyMoveDownText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Move_Down);
+        keyMoveLeftText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Move_Left);
+        keyMoveRightText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Move_Right);
+        keyInteractText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Interact);
+        keyInteractAlternate.text = GameInput.Instance.GetBindingText(GameInput.Binding.InteractAlternate);
+        keyPause.text = GameInput.Instance.GetBindingText(GameInput.Binding.Pause);
+        keyGamepadInteractText.text = GameInput.Instance.GetBindingText(GameInput.Binding.GamePad_Interact);
+        keyGamepadInteractAlternateText.text = GameInput.Instance.GetBindingText(GameInput.Binding.GamePad_InteractAlternate);
+        keyGamepadPause.text = GameInput.Instance.GetBindingText(GameInput.Binding.GamePad_Pause);
+
+    }
+
+    private void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
